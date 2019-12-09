c7b4b8c4860f751f932eb420c7e0069bc5d3a4cc
Nankk <toc32768@gmail.com>
2019-12-10 01:41:51 +0900

    :sparkles: Add dice roll powered by google search

--- src/dice_roll/core.cljs
+++ src/dice_roll/core.cljs
@@ -71,8 +71,7 @@
               target-elem (<p! (. page $ "#app"))
               _           (. page evaluate (fn [elem]
                                              (let [target (. js/document getElementById "app")]
-                                               (set! (. target -innerHTML) elem))) elem-txt)
-              ]
+                                               (set! (. target -innerHTML) elem))) elem-txt)]
           (println "Screenshot...")
           (<! (timeout 100)) ; What a nasty workaround! ("waitForNavigation" doesn't work in this case)
           (<p! (. page screenshot (clj->js {:path (str "./public/img/" out-name)
@@ -98,6 +97,64 @@
              (fn [err] (when err (. js/console log err) (. res sendStatus 400))))
           (. ps pipe res)))))
 
+;; Google ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defn- google-roll [dice]
+  (let [ch (chan)]
+    (go (let [out-name    "example.png"
+              browser     (<p! (. ppt launch (clj->js {;; :headless false
+                                                       :args ["--window-size=500,400"
+                                                              "--no-sandbox"
+                                                              "--disable-setuid-sandbox"]})))
+              page        (<p! (. browser newPage))
+              _           (<p! (. page goto (str "https://www.google.com/search?q="
+                                                 dice
+                                                 "&nirf=true" ; Avoid suggestion interruption
+                                                 )))]
+          (println "Screenshot Google search result...")
+          (<! (timeout 1100)) ; Google roll animation takes a while...
+          (<p! (. page screenshot (clj->js {:path (str "./public/img/" out-name)
+                                            :clip {:x      150
+                                                   :y      230
+                                                   :width  635
+                                                   :height 250}})))
+          (println "Screenshot!")
+          (. browser close)
+          (>! ch out-name)))
+    ch))
+
+(defn- return-roll-result [dice res]
+  (let [ch (chan)]
+    (go (let [png-name (<! (google-roll dice))
+              _  (println "Reading...")
+              r  (. fs createReadStream (str "./public/img/" png-name))
+              _  (println "Read!")
+              ps (. stream PassThrough)]
+          (. stream pipeline r ps
+             (fn [err] (when err (. js/console log err) (. res sendStatus 400))))
+          (. ps pipe res)))
+    ch))
+
+(defn- handle-roll-google [req res]
+  (println "handle-roll-google")
+  (go (try
+        (let [query      (js->clj (. req -query) :keywordize-keys true)
+              dice       (:dice query)
+              matches    (re-find #"([0-9]+)d([0-9]+)" dice)
+              dice-count (js/parseInt (matches 1))
+              dice-type  (js/parseInt (matches 2))]
+          (println dice)
+          (println dice-count)
+          (println dice-type)
+          (if (and (not (js/isNaN dice-count))
+                   (not (js/isNaN dice-type))
+                   (< dice-count 100)
+                   (some #(= % dice-type) '(4 6 8 10 12 20)))
+            (return-roll-result dice res)
+            (. (. res status 400) end)))
+        (catch js/Object e
+          (. (. res status 500) end)))))
+
 ;; Main ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (defonce server (atom nil))
@@ -118,6 +175,8 @@
     (. app get "/status" return-200) ; In order to keep the app active by robot access
 
     (. app get "/roll" return-rolled-dice)
+
+    (. app get "/roll-google" handle-roll-google)
 
     (when (some? @server)
       (. @server close)
